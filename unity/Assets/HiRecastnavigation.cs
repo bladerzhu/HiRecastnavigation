d1c6b6bd98c62312d4559cacf14b5b60f67ab3b7
Hiram <hiramtan@live.com>
2018-10-15 16:21:19 +0800

    add example

---
+++ unity/Assets/HiRecastnavigation.cs
@@ -0,0 +1,28 @@
+﻿
+using System.Runtime.InteropServices;
+
+namespace HiRecastnavigation
+{
+    public static class Navigation
+    {
+        private const string PluginsName = "HiRecastnavigation";
+
+        public static void Init()
+        {
+
+        }
+
+
+
+        #region glue
+
+        [DllImport(PluginsName)]
+        public static extern int TestCSharpCallC(int a, int b);
+
+
+        [DllImport(PluginsName)]
+        public static extern int TestCCallCSharp(int a, int b);
+
+        #endregion
+    }
+}
--- visual/HiRecastnavigation/HiRecastnavigation/HiRecastnavigationTest.h
+++
@@ -1,7 +0,0 @@
-#if defined (EXPORTBUILD)    
-# define _DLLExport __declspec (dllexport)    
-# else    
-# define _DLLExport __declspec (dllimport)    
-#endif    
-
-extern "C"  int _DLLExport MyADD(int x, int y);
